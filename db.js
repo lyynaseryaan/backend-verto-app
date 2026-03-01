b3431058312e710171fc7a8f106dcf5932e6c9ef
lyna <lyynserien@hotmail.com>
2026-03-01 23:32:25 +0100

    fix env variables

--- db.js
+++ db.js
@@ -1,18 +1,23 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
+
+console.log("MYSQLHOST:", process.env.MYSQLHOST);
+console.log("MYSQLUSER:", process.env.MYSQLUSER);
+console.log("MYSQLPORT:", process.env.MYSQLPORT);
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
 
 db.connect((err) => {
-    if (err) {
-        console.error("Database connection failed:", err);
-    } else {
-        console.log("MySQL Connected...");
-    }
+  if (err) {
+    console.log("Database connection failed:", err);
+  } else {
+    console.log("Connected to Database 🚀");
+  }
 });
 
 module.exports = db;
