6b2c0b5b920309c27ebf93bae317adcb7e6f9c04
lyna <lyynserien@hotmail.com>
2026-03-10 15:18:21 +0100

    update server.js

--- server.js
+++ server.js
@@ -4,16 +4,19 @@ const cors = require('cors');
 
 // Routes
 const authRoutes = require('./routes/auth');
-const levelRoutes = require('./routes/level'); // <-- استيراد فقط
+const levelRoutes = require('./routes/level');
+const quizRoutes = require('./routes/quiz');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+// routes
 app.use('/api/auth', authRoutes);
-app.use('/api', levelRoutes); // كل route في level.js يبدأ بـ /api
+app.use('/api', levelRoutes);
+app.use('/api', quizRoutes);
 
 app.listen(process.env.PORT, () => {
-    console.log('Server running on port ${process.env.PORT}');
+  console.log(`Server running on port ${process.env.PORT}`);
 });
