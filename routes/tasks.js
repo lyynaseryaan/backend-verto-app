7fbd3b6aaa883cf2b092cceb8932bb29bcf0ae28
wissem <wissemdjerboua9@gmail.com>
2026-03-15 10:24:09 +0100

    start task

--- routes/tasks.js
+++ routes/tasks.js
@@ -76,5 +76,54 @@ router.post('/add', (req, res) => {
     });
 
 });
+// ================= START TASK =================
+router.post('/start/:id', (req, res) => {
+
+  const authHeader = req.headers["authorization"];
+
+  if (!authHeader) {
+    return res.status(401).json({
+      success: false,
+      message: "No token provided"
+    });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+
+    if (err) {
+      return res.status(403).json({
+        success: false,
+        message: "Invalid token"
+      });
+    }
+
+    const userId = decoded.id;
+    const taskId = req.params.id;
+
+    const sql = 
+     " UPDATE tasks SET  start_time = NOW(), end_time = DATE_ADD(NOW(), INTERVAL duration MINUTE), status = 'running' WHERE id = ? AND user_id = ?";
+    
+
+    db.query(sql, [taskId, userId], (err, result) => {
+
+      if (err) {
+        return res.status(500).json({
+          success: false,
+          message: "Database error"
+        });
+      }
+
+      res.json({
+        success: true,
+        message: "Task started successfully"
+      });
+
+    });
+
+  });
+
+});
 
 module.exports = router;
