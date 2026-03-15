24a0c00accce0a93f5def3d669ecebe216cc1aad
wissem <wissemdjerboua9@gmail.com>
2026-03-15 10:15:01 +0100

    add taskes

---
+++ routes/tasks.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../db');
+const jwt = require('jsonwebtoken');
+
+
+// ================= ADD TASK =================
+router.post('/add', (req, res) => {
+
+    const authHeader = req.headers["authorization"];
+
+    if (!authHeader) {
+        return res.status(401).json({
+            success: false,
+            message: "No token provided"
+        });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+
+        if (err) {
+            return res.status(403).json({
+                success: false,
+                message: "Invalid token"
+            });
+        }
+
+        const userId = decoded.id;
+        const { title, duration } = req.body;
+
+        // check max 5 tasks
+        const checkSql = 
+        "SELECT COUNT(*) as total  FROM tasks  WHERE user_id = ? AND status != 'completed'";
+        
+
+        db.query(checkSql, [userId], (err, result) => {
+
+            if (err) {
+                return res.status(500).json({
+                    success: false,
+                    message: "Database error"
+                });
+            }
+
+            if (result[0].total >= 5) {
+                return res.status(400).json({
+                    success: false,
+                    message: "Maximum 5 tasks allowed"
+                });
+            }
+
+            const insertSql = 
+           " INSERT INTO tasks (user_id, title, duration) VALUES (?, ?, ?)";
+            
+
+            db.query(insertSql, [userId, title, duration], (err, result) => {
+
+                if (err) {
+                    return res.status(500).json({
+                        success: false,
+                        message: "Database error"
+                    });
+                }
+
+                res.status(201).json({
+                    success: true,
+                    message: "Task added successfully"
+                });
+
+            });
+
+        });
+
+    });
+
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const authRoutes = require('./routes/auth');
 const levelRoutes = require('./routes/level');
 const quizRoutes = require('./routes/quiz');
+const taskRoutes = require('./routes/tasks'); // ✅ أضفنا tasks
 
 const app = express();
 
@@ -16,7 +17,8 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api', levelRoutes);
 app.use('/api', quizRoutes);
+app.use('/api/tasks', taskRoutes); // ✅ route تاع tasks
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+  console.log('Server running on port ${process.env.PORT}');
 });
