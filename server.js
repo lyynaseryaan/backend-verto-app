f933366e42a3426e74149a1154f574c3d5b95494
wissem <wissemdjerboua9@gmail.com>
2026-03-31 22:33:21 +0200

    iuydtfguhki

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-
+app.use('/uploads', express.static('uploads'));
 // Routes
 const authRoutes = require('./routes/auth');
 const levelRoutes = require('./routes/level');
