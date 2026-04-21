e5340d63642f8f0660f3c1b993984e52430d4312
wissem <wissemdjerboua9@gmail.com>
2026-04-21 14:15:58 +0200

    update instroctooooors

--- routes/instructors.js
+++ routes/instructors.js
@@ -7,7 +7,7 @@
 
 const express  = require('express');
 const router   = express.Router();
-const db       = require('../../db');
+const db       = require('../db');
 const bcrypt   = require('bcryptjs');
 const jwt      = require('jsonwebtoken');
 
