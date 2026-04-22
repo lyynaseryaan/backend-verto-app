f1625725ea5c5fc19a67743285012175b489fb6f
wissem <wissemdjerboua9@gmail.com>
2026-04-23 01:57:33 +0200

    ya rbbbbbbb

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use('/api/admin',            adminProfileRoutes);
 app.use('/api/activity-feed',    activityFeedRoutes);
 app.use('/api/instructors',      instructorRoutes);
 app.use('/api/chatbot',          chatbotRoutes);
-app.use('/api/courses/overview', adminCoursesRoutes); // ✅
+app.use('/api/admin-courses', adminCoursesRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
